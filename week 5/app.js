df07bf90ebfcd8f1734141cc0ad655d577601b13
bartekringwelski <bartekr@gmail.com>
2015-09-23 15:59:27 -0700

    homework due

---
+++ week 5/app.js
@@ -0,0 +1,18 @@
+var num1 = 8;
+var num2 = 9;
+var num3 = 10;
+
+
+function max() {
+if(num1 > num2) {
+	alert("the biggest number is " +num1);
+}
+	else  alert(num2);
+}
+
+
+function maxOfThree() {
+	alert(Math.max(num1, num2, num3));
+}
+
+maxOfThree();
